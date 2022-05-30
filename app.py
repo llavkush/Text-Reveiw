dbf374cef31c3686ea6d0a49df4d7ac2f09c3279
Lavkush Gupta <lavkushgupt@live.in>
2022-05-30 14:39:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,10 @@ uploaded_file = st.file_uploader("Upload your input CSV file", type=["csv"])
 if uploaded_file is not None:
     #input_df = pd.read_csv(uploaded_file)
     chrome_reviews = pd.read_csv(uploaded_file)
+    chrome_reviews = chrome_reviews.loc[:, ['ID','Text','Star','User Name', 'Thumbs Up','Review URL','Developer Reply', 'Version', 'Review Date', 'App ID']]
+    st.write(First 3 Rows of Dataframe)
     st.write(chrome_reviews.head(3))
+    st.write(Please Wait for final Result.)
     #Cleaning Text of Emojis, Speacial Characters
     chrome_reviews['Text'].dropna()
     chrome_reviews['Tokenised_Text'] = chrome_reviews['Text'].apply(convert_emojis)
@@ -63,86 +66,11 @@ if uploaded_file is not None:
     chrome_reviews['Tokenised_Text'] = chrome_reviews['Tokenised_Text'].apply(to_lower)
     chrome_reviews['user_sentiments'] = chrome_reviews['Tokenised_Text'].apply(get_sentiments)
     chrome_reviews = chrome_reviews[chrome_reviews.user_sentiments == 'POSITIVE']
-    chrome_reviews = chrome_reviews[chrome_reviews_positive.Star < 3]
+    chrome_reviews = chrome_reviews[chrome_reviews.Star < 3]
     st.subheader('Output')
     chrome_reviews[['Text','user_sentiments','Star','Tokenised_Text']]
     st.write(chrome_reviews[['Text','user_sentiments','Star','Tokenised_Text']])
 else:
-    st.write("Please upload the CSV File")
-#     def user_input_features():
-#         pregnancies = st.sidebar.slider('No of Pregnancies',0,20,2)
-#         #sex = st.sidebar.selectbox('Sex',('male','female'))
-#         Glucose = st.sidebar.slider('Glucose', 0.0,400.0,120.0)
-#         BloodPressure = st.sidebar.slider('BloodPressure (mm Hg)', 0.00,150.00,69.10)
-#         SkinThickness = st.sidebar.slider('SkinThickness (mm)', 0.00,150.00,20.53)
-#         Insulin= st.sidebar.slider('Insulin (mu U/ml)', 0.00,1000.00,79.79)
-#         DiabetesPedigreeFunction = st.sidebar.slider('DiabetesPedigreeFunction (g)', -3.00,3.00,-1.01)
-#         Age = st.sidebar.slider('Age (Years)', 0,110,22)
-#         BMI = st.sidebar.slider('Body Mass Index ', 0.00,100.00,31.00)
-#         data = {'Pregnancy': pregnancies,
-#                 'Glucose': Glucose,
-#                 'BloodPressure': BloodPressure,
-#                 'SkinThickness': SkinThickness,
-#                 'Insulin': Insulin,
-#                 'BMI': BMI,
-#                 'DiabetesPedigreeFunction': DiabetesPedigreeFunction,
-#                 'Age': Age,
-#                 }
-#         features = pd.DataFrame(data, index=[0])
-#         return features
-#     input_df = user_input_features()
-# chrome_reviews = pd.read_csv(uploaded_file)
-# #Cleaning Text of Emojis, Speacial Characters
-# chrome_reviews['Text'].dropna()
-# chrome_reviews['Tokenised_Text'] = chrome_reviews['Text'].apply(convert_emojis)
-# chrome_reviews['Tokenised_Text'] = chrome_reviews['Text'].apply(clean)
-# chrome_reviews['Tokenised_Text'] = chrome_reviews['Tokenised_Text'].apply(is_special)
-# chrome_reviews['Tokenised_Text'] = chrome_reviews['Tokenised_Text'].apply(to_lower)
-# chrome_reviews['user_sentiments'] = chrome_reviews['Tokenised_Text'].apply(get_sentiments)
-
-# chrome_reviews = chrome_reviews[chrome_reviews.user_sentiments == 'POSITIVE']
-# chrome_reviews = chrome_reviews[chrome_reviews_positive.Star < 3]
-
-# st.subheader('Output')
-# chrome_reviews[['Text','user_sentiments','Star','Tokenised_Text']]
-# st.write(chrome_reviews[['Text','user_sentiments','Star','Tokenised_Text']])
-
-
-
-# # Combines user input features with entire Diabates dataset
-# diabates_raw = pd.read_csv('https://raw.githubusercontent.com/llavkush/Diabatic-Detection/Master/diabetes.csv')
-# diabates = diabates_raw.drop(columns='Outcome', axis=1, inplace = True)
-# df = pd.concat([input_df,diabates],axis=0)    
-    
-# df = df[:1] # Selects only the first row (the user input data)
-# #df1 = df.drop(columns="Outcome")
-# list = df.loc[:].values.tolist() # Converting df into lists
-# scaler = load(open('scaler.pkl', 'rb'))
-# features = scaler.transform(list)
-
-
-# # Displays the user input features
-# st.subheader('User Input features')
-
-# if uploaded_file is not None:
-#     st.write(df)
-# else:
-#     st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-#     st.write(df)
-
-# # Reads in saved classification model
-# load_clf = joblib.load(open('model.pkl', 'rb'))
-
-# # Apply model to make predictions
-# prediction = load_clf.predict(features)
-# prediction_proba = load_clf.predict_proba(features)
-
-# st.subheader('Prediction')
-# disease = np.array(['Non - Diabatic','Diabatic'])
-# st.write(disease[prediction])
-
-# st.subheader('Prediction Probability')
-# st.write(prediction_proba)
-
-
+    st.write("Please upload the CSV File with colunmns
+             ['ID','Text','Star','User Name', 'Thumbs Up','Review URL','Developer Reply', 'Version', 'Review Date', 'App ID']")
 st.subheader('Published By Lavkush Gupta')
